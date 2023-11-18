003c2e3694c8fc417297cae3213a13316cc55fca
mkan0141 <kanzume0141@gmail.com>
2023-11-19 00:21:03 +0900

    main chore(): add storybook

---
+++ src/components/spotify-iframe/index.stories.tsx
@@ -0,0 +1,34 @@
+import { Meta, StoryObj } from "storybook-framework-qwik";
+import { SpotifyIframe, type SpotifyIframeProps } from "./index";
+
+const meta: Meta<SpotifyIframeProps> = {
+  component: SpotifyIframe,
+};
+
+type Story = StoryObj<SpotifyIframeProps>;
+
+export default meta;
+
+export const Primary: Story = {
+  args: {
+    style: "border-radius:12px",
+    src: "https://open.spotify.com/embed/artist/1luOe8HkZQ7zwuaO2wuJqI?utm_source=generator",
+    sizeType: "default",
+    frameBorder: 0,
+    allowfullscreen: false,
+    allow:
+      "autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture",
+    loading: "lazy",
+  },
+  argTypes: {
+    sizeType: {
+      options: ["default", "compact"],
+      control: { type: "radio" },
+    },
+    allowfullscreen: {
+      options: [true, false],
+      control: { type: "radio" },
+    },
+  },
+  render: (props) => <SpotifyIframe {...props} />,
+};
